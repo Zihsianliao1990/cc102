a9c72c9619e3dbc8988757c4d56271da32598608
hsnuhsnu <hsnuhsmu@gmail.com>
2018-08-19 18:35:18 +0800

    Add existing file

---
+++ Downloads/mission3file/mission3.py
@@ -0,0 +1,35 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+iamInt = 0
+
+
+# In[2]:
+
+
+iamString = "我們要變很厲害"
+
+
+# In[4]:
+
+
+for num in range(iamInt, 10): 
+    print(num)
+
+
+# In[12]:
+
+
+intArray = [1,2,3,4,5]
+stringDict = { 'iamStringKey' : 'iamStringValue'}
+
+
+# In[13]:
+
+
+print(intArray[2])
+print(stringDict[ 'iamStringKey' ])
+
